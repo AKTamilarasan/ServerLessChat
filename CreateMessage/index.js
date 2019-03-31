33fdd42ec63fde87e7772b631a1e7b006a42c091
Ashok <ashok.mh90@gmail.com>
2019-03-31 17:28:57 +0530

    Deployed to azure

--- CreateMessage/index.js
+++ CreateMessage/index.js
@@ -1,4 +1,8 @@
 module.exports = async function (context, req) {
     context.bindings.cosmosDBMessage = req.body;
+    context.bindings.signalRMessages = [{
+        "target": "newMessage",
+        "arguments": [req.body]
+    }];
     context.done();
 };
